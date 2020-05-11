51b9e234cc062517520a1ef3a2968b86ea2e7252
Rahul Butani <rr.butani@gmail.com>
2020-05-11 08:43:16 -0500

    bindings: first pass at eFile

---
+++ inc/fat.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Assuming char has the same ABI as a uint8_t...
+
+extern void eFile_Init();
+
+extern void eFile_Mount(
+    uint8_t drive_num,
+    uint64_t size_in_sectors,
+);
+
+extern bool eFile_NewFile(
+    char const* path,
+    uint16_t len,
+);
+
+extern bool eFile_NewDir(
+    char const* path,
+    uint16_t len,
+);
+
+extern bool eFile_Read(
+    char const* path,
+    uint16_t len,
+    uint8_t buf[buf_len],
+    uint32_t buf_len,
+);
+
+extern bool eFile_ReadAll(
+    char const* path,
+    uint16_t len,
+    void (*func)(char),
+);
+
+extern bool eFile_Append(
+    char const* path,
+    uint16_t len,
+    uint8_t buf[buf_len],
+    uint32_t buf_len,
+);
+
+extern bool eFile_Delete(
+    char const* path,
+    uint16_t len,
+);
+
+extern bool eFile_DirList(
+    char const* path,
+    uint16_t len,
+    void (*func)(const char name[8], const char ext[3]),
+);
+
+extern bool eFile_Flush();
--- src/bindings.rs
+++ src/bindings.rs
@@ -35,6 +35,123 @@ pub extern "C" fn sector_sum(storage: &mut edisk::EDiskStorage, sector_num: u32)
     }
 }
 
+pub mod efile {
+    use super::edisk::EDiskStorage;
+    use crate::mutex::{Mutex, MutexInterface};
+    use crate::gpt::Gpt;
+    use crate::fat::FatFs;
+    use crate::fat::cache::eviction_policies::{LeastRecentlyAccessed, UnmodifiedFirst};
+    use crate::fat::dir::{DirIter, State};
+
+    use typenum::consts::{U512, U32, U16};
+
+    use core::slice::from_raw_parts;
+
+    static STORAGE: Mutex<Option<EDiskStorage>> = Mutex::new(None);
+    static FS: Mutex<Option<
+        FatFs<EDiskStorage, U16, UnmodifiedFirst<LeastRecentlyAccessed>>
+    >> = Mutex::new(None);
+
+    #[no_mangle]
+    pub extern "C" fn eFile_Init() { }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_Mount(drive_num: u8, size_in_sectors: u64) {
+        STORAGE.cs(|s| {
+            *s = Some(EDiskStorage { drive_num, size_in_sectors });
+
+            let s = s.as_mut().unwrap();
+
+            let g = Gpt::read_gpt(s).unwrap();
+            let p = g.get_partition_entry(s, 0).unwrap();
+
+            FS.cs(|f| {
+                *f = Some(FatFs::mount(s, &p,
+                    UnmodifiedFirst::<LeastRecentlyAccessed>::default()).unwrap()
+                );
+            })
+        })
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_NewFile(path: *const u8, len: u16) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        todo!()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_NewDir(path: *const u8, len: u16) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        todo!()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_Read(path: *const u8, len: u16, offset: u32, buf: *mut u8, buf_len: u32) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        todo!()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_ReadAll(path: *const u8, len: u16, func: extern "C" fn(u8)) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        todo!()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_Append(path: *const u8, len: u16, buf: *const u8, buf_len: u32) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        todo!()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_Delete(path: *const u8, len: u16) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        STORAGE.cs(|s| s.as_mut().map(|s| FS.cs(|f| f.as_mut().map(|f| {
+            if let Ok(p) = f.lookup_path(s, path) {
+                DirIter::from_cluster(f.root_dir_cluster_num, f, s)
+                    .delete(p)
+                    .is_ok()
+            } else {
+                false
+            }
+        })).unwrap_or(false)).unwrap_or(false))
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_DirList(path: *const u8, len: u16, func: extern "C" fn(*const u8, *const u8)) -> bool {
+        let path = unsafe { from_raw_parts(path, len as usize) };
+
+        STORAGE.cs(|s| s.as_mut().map(|s| FS.cs(|f| f.as_mut().map(|f| {
+            if let Ok((_, de)) = f.lookup_path(s, path) {
+                for (_, dir) in DirIter::from_cluster(de.cluster_idx(), f, s) {
+                    if let State::Exists = dir.state() {
+                        func(
+                            dir.file_name.0.as_ptr(),
+                            dir.file_ext.0.as_ptr(),
+                        )
+                    }
+                }
+                true
+            } else {
+                false
+            }
+        })).unwrap_or(false)).unwrap_or(false))
+    }
+
+    #[no_mangle]
+    pub extern "C" fn eFile_Flush() -> bool {
+        STORAGE.cs(|s| s.as_mut().map(|s| FS.cs(|f| f.as_mut().map(|f| {
+            f.cache.flush(s).is_ok()
+        })).unwrap_or(false)).unwrap_or(false))
+    }
+}
+
 pub mod edisk {
     use storage_traits::{Storage, errors::{ReadError, WriteError}};
     use generic_array::GenericArray;
