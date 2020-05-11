fafc586a615f6252146af05040ee2cbe32bdcb80
Rahul Butani <rr.butani@gmail.com>
2020-05-11 10:17:33 -0500

    bindings: misc

--- inc/fat.h
+++ inc/fat.h
@@ -9,48 +9,48 @@ extern void eFile_Init();
 
 extern void eFile_Mount(
     uint8_t drive_num,
-    uint64_t size_in_sectors,
+    uint64_t size_in_sectors
 );
 
 extern bool eFile_NewFile(
     char const* path,
-    uint16_t len,
+    uint16_t len
 );
 
 extern bool eFile_NewDir(
     char const* path,
-    uint16_t len,
+    uint16_t len
 );
 
 extern bool eFile_Read(
     char const* path,
     uint16_t len,
-    uint8_t buf[buf_len],
-    uint32_t buf_len,
+    uint8_t buf[/*buf_len*/],
+    uint32_t buf_len
 );
 
 extern bool eFile_ReadAll(
     char const* path,
     uint16_t len,
-    void (*func)(char),
+    void (*func)(char)
 );
 
 extern bool eFile_Append(
     char const* path,
     uint16_t len,
-    uint8_t buf[buf_len],
-    uint32_t buf_len,
+    uint8_t buf[/*buf_len*/],
+    uint32_t buf_len
 );
 
 extern bool eFile_Delete(
     char const* path,
-    uint16_t len,
+    uint16_t len
 );
 
 extern bool eFile_DirList(
     char const* path,
     uint16_t len,
-    void (*func)(const char name[8], const char ext[3]),
+    void (*func)(const char name[8], const char ext[3])
 );
 
 extern bool eFile_Flush();
